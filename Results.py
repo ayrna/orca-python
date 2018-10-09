ef8f0640e4237fb933a27cc42e107b2fb7f82392
beelzebub <i22bomui@github.com>
2018-10-09 21:45:55 +0200

    Classes and methods commented 'sklearn like'

--- Results.py
+++ Results.py
@@ -7,14 +7,34 @@ import pandas as pd
 class DataFrameStorage:
 
 	"""
+	DataFrameStorage
+
+	Stores all metrics scores (train and test) for a given combiantion of dataset and configuration.
+	It will contain a dataframe for each partition in which the dataset could be divided. If its not
+	partitionated, there'll be just one dataframe.
+	
+	Parameters
+	----------
+
+	dataset_name: string
+		Name of dataset used
+
+	configuration_name: string
+		Name of configuration used
+
+
+	Attributes
+	----------
+
+	df_: list of OrderedDict
+		Each dict contains the parameter's values with which the cross-validation metrics 
+		has been maximized (best parameters) during cross-validation phase, besides train
+		and test scores for all different metrics specified.
+		There will as dicts in the list as partitions the dataset is fragmented in.
 
 	"""
 
 	def __init__(self, dataset_name, configuration_name):
-
-		"""
-
-		"""
 
 		self.dataset_ = dataset_name
 		self.configuration_ = configuration_name
@@ -24,123 +44,155 @@ class DataFrameStorage:
 class Results:
 
 	"""
+	Results
+
+	Class that handles all info from an experiment that needs to be saved.
+	This info will be saved in CSV's inside a dedicated folder
+
+	Attributes
+	----------
+
+	dataframes_: list of DataFrameStorage objects
+		Each object will storage information about a pair of dataset-configuration.
+		There will be as many as the number of combinations of dataset and configurations.
+
 	"""
 
 	def __init__(self):
 
-		"""
-
-		"""
-
 		self.dataframes_ = []
 
 
-	def getDataFrame(self, dataset, configuration):
+	def getDataFrame(self, dataset_name, configuration_name):
 
 		"""
-			Method that looks if a dataframe for a given dataset and configuration already exists, if not, creates it
+		Look if a dataframe for a given dataset and configuration already exists,
+		if not, creates it
 
-			Receives:
+		Parameters
+		----------
 
-				- Dataset
-				- Configuration
+		dataset_name : string
+			Name of dataset used
 
-			Returns:
+		configuration_name : string
+			Name of configuration used
 
-				- DFS: DataFrameStorage object which contains information about execution from that combination
+		Returns
+		-------
+
+		dfs : DataFrameStorage object 
+			Contains (or will contain) train and test metrics for 'dataset' and
+			'configuration' given values
+
 		"""
 
+		# Searchs if this combination of 'dataset' and 'configuration' has already been used 
 		for dfs in self.dataframes_:
 
-			if dfs.dataset_ == dataset and dfs.configuration_ == configuration:
+			if dfs.dataset_ == dataset_name and dfs.configuration_ == configuration_name:
 				return dfs
 
-		dfs = DataFrameStorage(dataset, configuration)
+		# If the dataframe its not been added yet, creates it
+		dfs = DataFrameStorage(dataset_name, configuration_name)
 		self.dataframes_.append(dfs)
 
 		return dfs
 
 
 
-	def addRecord(self, dataset, configuration, train_metrics, test_metrics, best_params):
+	def addRecord(self, dataset_name, configuration_name, train_metrics, test_metrics, best_params):
 
 		"""
-			Stores all info about the run of a dataset with specified configuration.
+		Stores all info about the run of a dataset with a specified configuration.
 
-			The info will be stored as a pandas DataFrame in a class named DataFrameStorage built in
-			for the purpose of keeping additional information.
+		Parameters
+		----------
 
+		dataset_name: string
+			Name of dataset used
+
+		configuration_name: string
+			Name of configuration used
+
+		train_metrics: dictionary of floats
+			Dictionary with name of metrics as keys and scores as values
+
+		test_metrics: dictionary of floats
+			Dictionary with name of metrics as keys and scores as values
+
+		best_params: dictionary
+			Best parameters found during cross-validation from classifier and
+			parameters specified in this configuration.
 		"""
 
-		dfs = self.getDataFrame(dataset, configuration)
+		# Get or create a DataFrameStorage object for this dataset and configuration
+		dfs = self.getDataFrame(dataset_name, configuration_name)
+
 
 		dataframe_row = collections.OrderedDict()
-		# Adding best parameters as first columns in dataframe
+		# Adding best parameters as first elements in OrderedDict
 		for p_name, p_value in best_params.items():
 
 			# If some ensemble method has been used, then one of its parameters will be a dict containing
-			# the best parameters found for the internal algorithm
+			# the best parameters found for the meta classifier
 			if type(p_value) == dict:
 				for (k, v) in p_value.iteritems():
 					dataframe_row[k] = v
 			else:
 				dataframe_row[p_name] = p_value
 
-		# Concatenating train and test metrics
+
+		# Concatenating train and test metrics for easier display
 		for (tm_name, tm_value), (ts_name, ts_value) in zip(train_metrics.items(), test_metrics.items()):
 
 			dataframe_row[tm_name] = tm_value
 			dataframe_row[ts_name] = ts_value
 
+		# Adding this OrderedDict as a new entry to DataFrameStorage object
 		dfs.df_.append(dataframe_row)
 
 
-	def createSummary(self, df, avg_index, std_index):
+
+
+	def saveResults(self, fw_path, summary_index, metrics_names):
 
 		"""
-			Summarizing information from all partitions of configuration into one line
+		Method used for saving experiment info to CSV's.
+
+		By default, there will be a dedicated subfolder inside framework's one.
+
+		Each time a experiment has been run successfully, this method will generate
+		a new subfolder inside that subfolder, named 'exp-YY-MM-DD-hh-mm-ss'.
+
+		This new generated folder will store the train and test summaries as CSV, 
+		as well as so many subfolders as datasets, named after them.
+
+		Last, inside this dataset subfolders, there will be one CSV for each
+		configuration used, containing info about metrics and partitions.
+
 
 		"""
 
-		# Dissociating train and test metrics
+		# TODO: If allowed to change folder name, check all problems derived form it: ending in backslash,
+		# 		whether folder is specified from root or relative to framework folder, etc
 
-		n_parameters = len(df.columns) - len(avg_index)*2	#Number of parameters used in this configuration
-		train_df = df.iloc[:,n_parameters::2].copy() 		#Even columns from dataframe (train metrics)
-		test_df = df.iloc[:,(n_parameters+1)::2].copy()		#Odd columns (test metrics)
+		# 		It may be better to pass it as a parameter
 
-		# Computing mean and standard deviation for metrics
-		train_avg, train_std = train_df.mean(), train_df.std()
-		test_avg, test_std = test_df.mean(), test_df.std()
-		# Naming indexes for summary dataframes
-		train_avg.index, train_std.index = avg_index, std_index
-		test_avg.index, test_std.index = avg_index, std_index
-		# Merging avg and std into one dataframe
-		train_summary_row = pd.concat([train_avg, train_std])
-		test_summary_row = pd.concat([test_avg, test_std])
-
-		# Mixing avg and std dataframe columns results from metrics summaries
-		train_summary_row = train_summary_row[list(sum(zip(	train_summary_row.iloc[:len(avg_index)].keys(),\
-											 				train_summary_row.iloc[len(std_index):].keys()), ()))]
-		test_summary_row = test_summary_row[list(sum(zip(test_summary_row.iloc[:len(avg_index)].keys(),\
-											 			 test_summary_row.iloc[len(std_index):].keys()), ()))]
-		return train_summary_row, test_summary_row
+		runs_folder = "my_runs/"
 		
 
-	def saveResults(self, api_path, summary_index, metrics_names):
+		# TODO: Try-catch and raising exceptions if needed when two names of experiment folders collide
 
-		"""
-
-		"""
-		
 		# Check if experiments folder exists
-		if not os.path.exists(api_path + "my_runs/"):
-			os.makedirs(api_path + "my_runs/")
+		if not os.path.exists(fw_path + runs_folder):
+			os.makedirs(fw_path + runs_folder)
 
 		# Getting name of folder where we will store info about the Experiment
 		folder_name = "exp-" + datetime.date.today().strftime("%y-%m-%d") + "-" + datetime.datetime.now().strftime("%H-%M-%S") + "/"
 
 		# Check if folder already exists
-		folder_path = api_path + "my_runs/" + folder_name
+		folder_path = fw_path + runs_folder + folder_name
 		if not os.path.exists(folder_path):
 			os.makedirs(folder_path)
 
@@ -174,6 +226,67 @@ class Results:
 		test_summary.to_csv(folder_path + "/" + "test_summary.csv")
 
 
+
+
+	def createSummary(self, df, avg_index, std_index):
+
+		"""
+		Summarizing information from all partitions stored in a DataFrameStorage 
+		object into one line of a DataFrame
+
+
+		Parameters
+		----------
+
+			df: DataFrame
+				Object that stores train and test metrics, as well as the parameters
+				used to obtain them, for all partitions of a dataset with a given
+				configuration
+
+			avg_index: list of strings
+				Includes all names of metrics calculated ending with '_mean'
+
+			std_index: list of strings
+				Includes all names of metrics calculated ending with '_std'
+			
+
+		Returns
+		-------
+	
+			train_summary_row: DataFrame
+				DataFrame with only one row, containing mean and standard deviation
+				for all metrics calculated across partitions.
+
+				Initial column will indicate dataset-configuration pair from which
+				we are summarizing info from.
+
+			test_summary_row: DataFrame
+				Simmilar to train_summary_row, but storing only info about test scores
+
+		"""
+
+		# Dissociating train and test metrics
+		n_parameters = len(df.columns) - len(avg_index)*2	#Number of parameters used in this configuration
+		train_df = df.iloc[:,n_parameters::2].copy() 		#Even columns from dataframe (train metrics)
+		test_df = df.iloc[:,(n_parameters+1)::2].copy()		#Odd columns (test metrics)
+
+		# Computing mean and standard deviation for metrics
+		train_avg, train_std = train_df.mean(), train_df.std()
+		test_avg, test_std = test_df.mean(), test_df.std()
+		# Naming indexes for summary dataframes
+		train_avg.index, train_std.index = avg_index, std_index
+		test_avg.index, test_std.index = avg_index, std_index
+		# Merging avg and std into one dataframe
+		train_summary_row = pd.concat([train_avg, train_std])
+		test_summary_row = pd.concat([test_avg, test_std])
+
+		# Mixing avg and std dataframe columns results from metrics summaries
+		train_summary_row = train_summary_row[list(sum(zip(	train_summary_row.iloc[:len(avg_index)].keys(),\
+											 				train_summary_row.iloc[len(std_index):].keys()), ()))]
+		test_summary_row = test_summary_row[list(sum(zip(test_summary_row.iloc[:len(avg_index)].keys(),\
+											 			 test_summary_row.iloc[len(std_index):].keys()), ()))]
+		return train_summary_row, test_summary_row
+		
 
 
 
