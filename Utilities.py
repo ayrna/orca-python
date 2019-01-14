52055f2c4fb30abf95a42f3404e963b984a71633
gdc <i22bomui@github.com>
2019-01-14 16:47:23 +0100

    Dinamical load of classifiers has been modified. Seed controlled via Sacred again.

--- Utilities.py
+++ Utilities.py
@@ -18,7 +18,7 @@ class Utilities:
 	Class in charge of running an experiment over N datasets, where we
 	apply M different configurations over each one of them.
 
-	Configurations are composed of an algorithm and different parameters,
+	Configurations are composed of a classifier and different parameters,
 	where it may be multiple values for every parameter.
 
 	This function will find, for all dataset-configuration pairs, the best
@@ -79,11 +79,14 @@ class Utilities:
 
 
 		self.results_ = Results()
-		# Adding algorithm folder to sys path. Needed to import modules from different folders
-		sys.path.insert(0, 'Algorithms/')
+		# Adding classifier folder to sys path. Needed to import modules from different folders
+		sys.path.insert(0, 'Classifiers/')
 
-		if 'seed' in self.configurations_:
-			np.random.seed(self.configurations_['seed'])
+		"""
+		if 'seed' in self.general_conf_:
+			np.random.seed(self.general_conf_['seed'])
+		"""
+
 
 		self._checkDatasetList()
 
@@ -114,22 +117,17 @@ class Utilities:
 				print "Running", conf_name, "..."
 
 
-				# Loading Algorithm stated in configuration
-				algorithm = self._loadAlgorithm(configuration["algorithm"])
+				# Loading Classifier stated in configuration
+				classifier = self._loadClassifier(configuration["classifier"])
 				# Iterating over all partitions in each dataset
 				for idx, partition in enumerate(dataset):
-
-
-					# TODO: Si los ficheros no estan nombrados apropiadamente, entonces no se correspondera
-					#		con las particiones que deberia
-
 
 					# Print actual running partition
 					print "  Running Partition", idx
 
 					# Finding optimal parameters
 					optimal_estimator = self._getOptimalEstimator(partition["train_inputs"], partition["train_outputs"],\
-																  algorithm, configuration["parameters"])
+																  classifier, configuration["parameters"])
 
 
 					# Creating tuples with each specified tuple and passing it to specified dataframe
@@ -165,13 +163,21 @@ class Utilities:
 
 						else:
 
+							test_predicted_y = None
 							test_metrics[metric_name.strip() + '_test'] = np.nan
 
 
 
+					# Saving time taken to cross-validate hyperparameters and re-fitting time of best model
+					train_metrics['cv_time_train'] = optimal_estimator.cv_results_['mean_fit_time'].sum()
+					test_metrics['cv_time_test'] = optimal_estimator.cv_results_['mean_score_time'].sum()
+					train_metrics['refit_time'] = optimal_estimator.refit_time_
+
 					# Save metrics scores for this partition
-					self.results_.addRecord(dataset_name, conf_name, train_metrics, test_metrics,\
-											optimal_estimator.best_params_)
+					self.results_.addRecord(idx, optimal_estimator.best_params_, optimal_estimator.best_estimator_,\
+											{'dataset': dataset_name, 'config': conf_name},\
+											{'train': train_metrics, 'test': test_metrics},\
+											{'train': train_predicted_y, 'test': test_predicted_y})
 
 
 
@@ -232,11 +238,15 @@ class Utilities:
 
 		try:
 
-			# Creating dicts for all partitions (saving order between partitions as keys)
+			# TODO: Usar mejor el nombre completo del fichero (sin la parte de train) ??
+
+
+			# Creating dicts for all partitions (saving partition order as keys)
 			partition_list = { filename[ filename.find('.') + 1 : ]: {} for filename in os.listdir(dataset_path)\
 								if filename.startswith("train_") }
 
-			# Saving info of every dataset file
+
+			# Saving info for every dataset file
 			for filename in os.listdir(dataset_path):
 
 				if filename.startswith("train_"):
@@ -259,10 +269,9 @@ class Utilities:
 			raise RuntimeError("Found partition without train files: partition %s" % filename[ filename.find('.') + 1 : ])
 
 
-		# Saving partitions as a sorted list of dicts
+		# Saving partitions as a sorted list of dicts (according to it's partition order)
 		partition_list = list( collections.OrderedDict(sorted(partition_list.items(), key=(lambda t: self._getKey(t[0])))).values() )
 
-		# Save info to dataset
 		return partition_list
 
 
@@ -292,6 +301,7 @@ class Utilities:
 
 		"""
 
+		# Separator is automatically found
 		f = pd.read_csv(filename, header=None, engine='python', sep=None)
 
 		inputs = f.values[:,0:(-1)]
@@ -338,7 +348,7 @@ class Utilities:
 		self.general_conf_['datasets'] = dataset_list
 
 
-	def _loadAlgorithm(self, algorithm_path):
+	def _loadClassifier(self, classifier_path):
 
 		"""
 		Loads and returns a classifier.
@@ -346,7 +356,7 @@ class Utilities:
 		Parameters
 		----------
 
-		algorithm_path: string
+		classifier_path: string
 			Relative path to which package the classifier class is located in.
 			That module can be local if the classifier is built inside the
 			framework, or relative to scikit-learn package.
@@ -355,41 +365,34 @@ class Utilities:
 		Returns
 		-------
 
-		algorithm: object
+		classifier: object
 			Returns a loaded classifier, either from an scikit-learn module, or from
 			a module of this framework.
 
 		"""
 
-		# Loading modules to execute algorithm given in configuration file
-		modules = [x for x in algorithm_path.split('.')]
+		try:
 
-		if (len(modules) == 1):
+			if (len(classifier_path.split('.')) == 1):
 
-			try:
-				algorithm = __import__(modules[0])
-				algorithm = getattr(algorithm, modules[0])
+				classifier = __import__(classifier_path)
+				classifier = getattr(classifier, classifier_path)
 
-			except:
-				raise ImportError("Unable to load classifier's path: %s" % algorithm_path)
+			else:
 
-		elif (len(modules) == 3):
+				classifier = __import__(classifier_path.rsplit('.', 1)[0], fromlist="None")
+				classifier = getattr(classifier, classifier_path.rsplit('.', 1)[1])
 
-			try:
-				algorithm = __import__(modules[0] + '.' + modules[1], fromlist=[str(modules[2])])
-				algorithm = getattr(algorithm, modules[2])
-
-			except:
-				raise ImportError("Unable to load classifier's path: %s" % algorithm_path)
-
-		else:
-			raise AttributeError("Unable to load classifier's path: %s" % algorithm_path)
-
-		return algorithm
+		except ImportError:
+			raise ImportError("Unable to load classifier's path: %s" % classifier_path)
 
 
 
-	def _getOptimalEstimator(self, train_inputs, train_outputs, algorithm, parameters):
+		return classifier
+
+
+
+	def _getOptimalEstimator(self, train_inputs, train_outputs, classifier, parameters):
 
 		"""
 		Perform cross-validation technique for finding best
@@ -413,20 +416,24 @@ class Utilities:
 			over train samples from given partition (or dataset)
 		"""
 
-		try:		
+		try:
 			module = __import__("Metrics")
 			metric = getattr(module, self.general_conf_['cv_metric'].lower().strip())
 
 		except AttributeError:
+
+			if type(self.general_conf_['cv_metric']) == list:
+				raise AttributeError("Cross-Validation Metric must be a string")
+
 			raise AttributeError("No metric named '%s'" % self.general_conf_['cv_metric'].strip().lower())
 
 		gib = module.greater_is_better(self.general_conf_['cv_metric'].lower().strip())
 		scoring_function = make_scorer(metric, greater_is_better=gib)
-		# Checking if this configuration uses OrdinalDecomposition algorithm
+		# Checking if this configuration uses OrdinalDecomposition classifier
 
 		parameters = self._extractParams(parameters)
 
-		optimal = GridSearchCV(estimator=algorithm(), param_grid=parameters, scoring=scoring_function,\
+		optimal = GridSearchCV(estimator=classifier(), param_grid=parameters, scoring=scoring_function,\
 					n_jobs=self.general_conf_['jobs'], cv=self.general_conf_['folds'], iid=False)
 
 		optimal.fit(train_inputs, train_outputs)
@@ -472,18 +479,28 @@ class Utilities:
 			# we need to transform a dict of lists, into a list of dicts.
 			elif (type(param) == dict) and (param_name == 'parameters'):
 
-				# Creating a list for each parameter. Elements represented as 'parameterName_parameterValue'.
-				p_list = [ [p_name + '_' + str(v) for v in p] for p_name, p in param.iteritems() ]
-				# Permutations of all lists. Generates all possible combination of elements between lists.
-				p_list = [ list(item) for item in list(product(*p_list)) ]
-				# Creates a list of dictionaries, containing all combinations of given parameters
-				p_list = [ dict( [item.split('_') for item in p] ) for p in p_list ]
+				try:
 
+					#TODO: Si hay algun parametro que contenga guiones, falla (se esta usando para representar los valores)
+
+					# Creating a list for each parameter. Elements represented as 'parameterName-parameterValue'.
+					p_list = [ [p_name + '-' + str(v) for v in p] for p_name, p in param.iteritems() ]
+					# Permutations of all lists. Generates all possible combination of elements between lists.
+					p_list = [ list(item) for item in list(product(*p_list)) ]
+					# Creates a list of dictionaries, containing all combinations of given parameters
+					p_list = [ dict( [item.split('-') for item in p] ) for p in p_list ]
+
+				except TypeError:
+					raise TypeError('All parameters for the inner classifier must be an iterable object')
+
+				# TODO: It has to be a better way of doing this without going through all elements again
 				# Returns non-string values back to it's normal self
 				for d in p_list:
 					for (k, v) in d.iteritems():
 
-						if self._isFloat(v):
+						if self._isInt(v):	#TODO: Solamente se usa para random_state realmente (no admite floats)
+							d[k] = int(v)
+						elif self._isFloat(v):
 							d[k] = float(v)
 						elif self._isBoolean(v):
 							d[k] = bool(v)
@@ -493,6 +510,25 @@ class Utilities:
 		return parameters
 
 
+	def _isInt(self, value):
+
+		"""
+		Check if an string can be converted to int
+
+		Parameters
+		----------
+		value: string
+
+		Returns
+		-------
+		Int
+		"""
+
+		try:
+			int(value)
+			return True
+		except ValueError:
+			return False
 
 	def _isFloat(self, value):
 
@@ -505,7 +541,7 @@ class Utilities:
 
 		Returns
 		-------
-		Boolean
+		Float
 		"""
 
 		try:
@@ -559,17 +595,16 @@ class Utilities:
 
 
 
-	def writeReport(self, fw_path):
+	def writeReport(self):
 
 		"""
-		Saves information about experiment run into CSVs
+		Saves information about experiment through Results class
 
-		Parameters
-		----------
-
-		fw_path: string
-			Path to framework folder
 		"""
+
+
+		print "\nSaving Results..."
+
 
 		# Info needed to save execution info properly
 		# All possible pairs of dataset-configuration names
@@ -583,6 +618,6 @@ class Utilities:
 		# Names of each metric used
 		metrics_names = [x.strip().lower() for x in self.general_conf_['metrics']]
 
-		self.results_.saveResults(fw_path, summary_index, metrics_names)
+		self.results_.saveResults(self.general_conf_['runs_folder'], summary_index, metrics_names)
 
 
