89f1680788b70584562be87208c485a985a2a8b5
i22bomui <i22bomui@uco.es>
2020-04-03 13:47:27 +0200

    Added exceptionns handling "non-configured" experiments

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def default_config():
 
 	# Giving default values
 	general_conf = {"basedir": "",
-					"dataset": "",
+					"datasets": "",
 					"hyperparam_cv_nfolds": 3,
 					"jobs": 1,
 					"metrics": "ccr",
@@ -17,9 +17,21 @@ def default_config():
 					"output_folder": "my_runs/"
 					}
 
+	configurations = {}
 
 @ex.automain
 def main(general_conf, configurations):
+
+	if not general_conf['basedir'] or not general_conf['datasets']:
+
+		raise RuntimeError('A dataset has to be defined to run this program.\n' +
+							'For more information about using this framework, please refer to the README.')
+
+	if not configurations:
+
+		raise RuntimeError('No configuration was defined.\n' + 
+							'For more information about using this framework, please refer to the README.')
+
 
 	interface = Utilities(general_conf, configurations)
 	interface.run_experiment()
