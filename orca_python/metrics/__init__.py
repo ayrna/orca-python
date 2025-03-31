06d608c0b10c077ca5d9ef28fb6a60b226c3b465
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2025-03-31 10:46:34 +0200

    Add __init__.py to metrics module
    Create __init__.py in metrics module to enable package structure.

---
+++ orca_python/metrics/__init__.py
@@ -0,0 +1,29 @@
+"""Metrics module."""
+
+from .metrics import (
+    greater_is_better,
+    ccr,
+    amae,
+    gm,
+    mae,
+    mmae,
+    ms,
+    mze,
+    tkendall,
+    wkappa,
+    spearman,
+)
+
+__all__ = [
+    "greater_is_better",
+    "ccr",
+    "amae",
+    "gm",
+    "mae",
+    "mmae",
+    "ms",
+    "mze",
+    "tkendall",
+    "wkappa",
+    "spearman",
+]
