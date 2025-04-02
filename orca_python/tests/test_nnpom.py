b8bedcbbf6964dfab1d2f0933a8027b556d870c9
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2025-04-02 10:34:29 +0200

    Migrate test_nnpom to pytest

--- orca_python/tests/test_nnpom.py
+++ orca_python/tests/test_nnpom.py
@@ -1,8 +1,7 @@
 from sys import path as syspath
 from os import path as ospath
 
-import unittest
-
+import pytest
 import numpy as np
 import numpy.testing as npt
 
@@ -12,118 +11,114 @@ import numpy.testing as npt
 from orca_python.classifiers.NNPOM import NNPOM
 
 
-class TestNNPOM(unittest.TestCase):
-	"""
-	Class testing NNPOM's functionality.
+@pytest.fixture
+def dataset_path():
+    return ospath.join(ospath.dirname(ospath.abspath(__file__)), "test_datasets", "test_nnpom_nnop_load_dataset", "toy")
 
-	This classifier is built in classifiers/NNPOM.py.
-	"""
+@pytest.fixture
+def train_file(dataset_path):
+    return np.loadtxt(ospath.join(dataset_path,"train_toy.0"))
 
-	dataset_path = ospath.join(ospath.dirname(ospath.abspath(__file__)), "test_datasets", "test_nnpom_nnop_load_dataset", "toy")
+@pytest.fixture
+def test_file(dataset_path):
+    return np.loadtxt(ospath.join(dataset_path,"test_toy.0"))
 
-	train_file = np.loadtxt(ospath.join(dataset_path,"train_toy.0"))
-	test_file = np.loadtxt(ospath.join(dataset_path,"test_toy.0"))
+#	-----	NOT APPLIED	-----
+# It doesn't apply to the because can't set seed to randomize model weights.
+# def test_nnpom_fit_correct(self):
+# 	#Check if this algorithm can correctly classify a toy problem.
+	
+# 	#Test preparation
+# 	X_train = self.train_file[:,0:(-1)]
+# 	y_train = self.train_file[:,(-1)]
 
-	#	-----	NOT APPLIED	-----
-	# It doesn't apply to the because can't set seed to randomize model weights.
-	# def test_nnpom_fit_correct(self):
-	# 	#Check if this algorithm can correctly classify a toy problem.
-		
-	# 	#Test preparation
-	# 	X_train = self.train_file[:,0:(-1)]
-	# 	y_train = self.train_file[:,(-1)]
+# 	X_test = self.test_file[:,0:(-1)]
+	
+# 	expected_predictions = [ospath.join(self.dataset_path,"expectedPredictions.0")]
+# 							# ospath.join(self.dataset_path,"expectedPredictions.1"),
+# 							# ospath.join(self.dataset_path,"expectedPredictions.2"),
+# 							# ospath.join(self.dataset_path,"expectedPredictions.3")]
 
-	# 	X_test = self.test_file[:,0:(-1)]
-		
-	# 	expected_predictions = [ospath.join(self.dataset_path,"expectedPredictions.0")]
-	# 							# ospath.join(self.dataset_path,"expectedPredictions.1"),
-	# 							# ospath.join(self.dataset_path,"expectedPredictions.2"),
-	# 							# ospath.join(self.dataset_path,"expectedPredictions.3")]
+# 	classifiers = [NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)]
 
-	# 	classifiers = [NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)]
-
-	# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 20, iterations = 500, lambdaValue = 0.01),
-	# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 250, lambdaValue = 0.01),
-	# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 20, iterations = 500, lambdaValue = 0.01)]
-
-
-	# 	#Test execution and verification
-	# 	for expected_prediction, classifier in zip(expected_predictions, classifiers):
-	# 		classifier.fit(X_train, y_train)
-	# 		predictions = classifier.predict(X_test)
-	# 		expected_prediction = np.loadtxt(expected_prediction)
-	# 		npt.assert_equal(predictions, expected_prediction, "The prediction doesnt match with the desired values")
-
-	def test_nnpom_fit_not_valid_parameter(self):
-
-		#Test preparation
-		X_train = self.train_file[:,0:(-1)]
-		y_train = self.train_file[:,(-1)]
-
-		classifiers = [NNPOM(epsilonInit=0.5, hiddenN=-1, iterations=1000, lambdaValue=0.01),
-						NNPOM(epsilonInit=0.5, hiddenN=10, iterations=-1, lambdaValue=0.01)]
-
-		#Test execution and verification
-		for classifier in classifiers:
-				model = classifier.fit(X_train, y_train)
-				self.assertIsNone(model, "The NNPOM fit method doesnt return Null on error")
-
-	def test_nnpom_fit_not_valid_data(self):
-		#Test preparation
-		X_train = self.train_file[:,0:(-1)]
-		y_train = self.train_file[:,(-1)]
-		X_train_broken = self.train_file[0:(-1),0:(-2)]
-		y_train_broken = self.train_file[0:(-1),(-1)]
-
-		#Test execution and verification
-		classifier = NNPOM(epsilonInit=0.5, hiddenN=10, iterations=1000, lambdaValue=0.01)
-		with self.assertRaises(ValueError):
-				model = classifier.fit(X_train, y_train_broken)
-				self.assertIsNone(model, "The NNPOM fit method doesnt return Null on error")
-
-		with self.assertRaises(ValueError):
-				model = classifier.fit([], y_train)
-				self.assertIsNone(model, "The NNPOM fit method doesnt return Null on error")
-
-		with self.assertRaises(ValueError):
-				model = classifier.fit(X_train, [])
-				self.assertIsNone(model, "The NNPOM fit method doesnt return Null on error")
-
-		with self.assertRaises(ValueError):
-				model = classifier.fit(X_train_broken, y_train)
-				self.assertIsNone(model, "The NNPOM fit method doesnt return Null on error")
+# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 20, iterations = 500, lambdaValue = 0.01),
+# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 250, lambdaValue = 0.01),
+# 	#			   NNPOM(epsilonInit = 0.5, hiddenN = 20, iterations = 500, lambdaValue = 0.01)]
 
 
-	#	-----	NOT APPLIED	-----
-	# It doesn't apply to the because it has no internal model
-	# like in other classifiers like REDSVM or SVOREX. 
-	# def test_nnpom_model_is_not_a_dict(self):
-	# 	#Test preparation
-	# 	X_train = self.train_file[:,0:(-1)]
-	# 	y_train = self.train_file[:,(-1)]
+# 	#Test execution and verification
+# 	for expected_prediction, classifier in zip(expected_predictions, classifiers):
+# 		classifier.fit(X_train, y_train)
+# 		predictions = classifier.predict(X_test)
+# 		expected_prediction = np.loadtxt(expected_prediction)
+# 		npt.assert_equal(predictions, expected_prediction, "The prediction doesnt match with the desired values")
 
-	# 	X_test = self.test_file[:,0:(-1)]
+def test_nnpom_fit_not_valid_parameter(train_file):
+    #Test preparation
+    X_train = train_file[:,0:(-1)]
+    y_train = train_file[:,(-1)]
 
-	# 	classifier = NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)
-	# 	classifier.fit(X_train, y_train)
+    classifiers = [NNPOM(epsilonInit=0.5, hiddenN=-1, iterations=1000, lambdaValue=0.01),
+                    NNPOM(epsilonInit=0.5, hiddenN=10, iterations=-1, lambdaValue=0.01)]
 
-	# 	#Test execution and verification
-	# 	with self.assertRaisesRegex(TypeError, "Model should be a dictionary!"):
-	# 			classifier.classifier_ = 1
-	# 			classifier.predict(X_test)
+    #Test execution and verification
+    for classifier in classifiers:
+            model = classifier.fit(X_train, y_train)
+            assert model is None, "The NNPOM fit method doesnt return Null on error"
+
+def test_nnpom_fit_not_valid_data(train_file):
+    #Test preparation
+    X_train = train_file[:,0:(-1)]
+    y_train = train_file[:,(-1)]
+    X_train_broken = train_file[0:(-1),0:(-2)]
+    y_train_broken = train_file[0:(-1),(-1)]
+
+    #Test execution and verification
+    classifier = NNPOM(epsilonInit=0.5, hiddenN=10, iterations=1000, lambdaValue=0.01)
+    with pytest.raises(ValueError):
+            model = classifier.fit(X_train, y_train_broken)
+            assert model is None, "The NNPOM fit method doesnt return Null on error"
+
+    with pytest.raises(ValueError):
+            model = classifier.fit([], y_train)
+            assert model is None, "The NNPOM fit method doesnt return Null on error"
+
+    with pytest.raises(ValueError):
+            model = classifier.fit(X_train, [])
+            assert model is None, "The NNPOM fit method doesnt return Null on error"
+
+    with pytest.raises(ValueError):
+            model = classifier.fit(X_train_broken, y_train)
+            assert model is None, "The NNPOM fit method doesnt return Null on error"
 
 
-	def test_nnpom_predict_not_valid_data(self):
-		#Test preparation
-		X_train = self.train_file[:,0:(-1)]
-		y_train = self.train_file[:,(-1)]
+#	-----	NOT APPLIED	-----
+# It doesn't apply to the because it has no internal model
+# like in other classifiers like REDSVM or SVOREX. 
+# def test_nnpom_model_is_not_a_dict(self):
+# 	#Test preparation
+# 	X_train = self.train_file[:,0:(-1)]
+# 	y_train = self.train_file[:,(-1)]
 
-		classifier = NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)
-		classifier.fit(X_train, y_train)
+# 	X_test = self.test_file[:,0:(-1)]
 
-		#Test execution and verification
-		with self.assertRaises(ValueError):
-			classifier.predict([])
+# 	classifier = NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)
+# 	classifier.fit(X_train, y_train)
 
-if __name__ == '__main__':
-	unittest.main()
+# 	#Test execution and verification
+# 	with self.assertRaisesRegex(TypeError, "Model should be a dictionary!"):
+# 			classifier.classifier_ = 1
+# 			classifier.predict(X_test)
+
+
+def test_nnpom_predict_not_valid_data(train_file):
+    #Test preparation
+    X_train = train_file[:,0:(-1)]
+    y_train = train_file[:,(-1)]
+
+    classifier = NNPOM(epsilonInit = 0.5, hiddenN = 10, iterations = 500, lambdaValue = 0.01)
+    classifier.fit(X_train, y_train)
+
+    #Test execution and verification
+    with pytest.raises(ValueError):
+        classifier.predict([])
