8d83fe2f9cb6471edd1eff919fe6a1315925789c
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-04-21 21:01:59 +0200

    CI: Replace black and isort with ruff-format in pre-commit

--- config.py
+++ config.py
@@ -7,16 +7,13 @@ ex = Experiment("Experiment Configuration")
 
 @ex.automain
 def main(general_conf, configurations):
-
     if not general_conf["basedir"] or not general_conf["datasets"]:
-
         raise RuntimeError(
             "A dataset has to be defined to run this program.\n"
             + "For more information about using this framework, please refer to the README."
         )
 
     if not configurations:
-
         raise RuntimeError(
             "No configuration was defined.\n"
             + "For more information about using this framework, please refer to the README."
