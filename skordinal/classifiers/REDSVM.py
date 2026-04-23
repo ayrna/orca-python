990a767c5a6af480f9946950ef0b1d85720b55a1
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-04-23 11:55:59 +0200

    REF: Rename package

---
+++ skordinal/classifiers/REDSVM.py
@@ -0,0 +1,232 @@
+"""Reduction from ordinal regression to binary SVM (REDSVM)."""
+
+from numbers import Integral, Real
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin, _fit_context
+from sklearn.utils._param_validation import Interval, StrOptions
+from sklearn.utils.multiclass import unique_labels
+from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
+
+from skordinal.classifiers.libsvmRank.python import svm  # type: ignore[attr-defined]
+
+
+class REDSVM(BaseEstimator, ClassifierMixin):
+    """Reduction from ordinal regression to binary SVM classifiers.
+
+    The configuration used is the identity coding matrix, the absolute cost matrix and
+    the standard binary soft-margin SVM. This class uses libsvm-rank-2.81
+    implementation: (http://www.work.caltech.edu/~htlin/program/libsvm/)
+
+    Parameters
+    ----------
+    C : float, default=1
+        Set the parameter C.
+
+    kernel : str, default="rbf"
+        Set type of kernel function.
+        - linear: u'*v
+        - polynomial: (gamma*u'*v + coef0)^degree
+        - rbf: exp(-gamma*|u-v|^2)
+        - sigmoid: tanh(gamma*u'*v + coef0)
+        - stump: -|u-v|_1 + coef0
+        - perceptron: -|u-v|_2 + coef0
+        - laplacian: exp(-gamma*|u-v|_1)
+        - exponential: exp(-gamma*|u-v|_2)
+        - precomputed: kernel values in training_instance_matrix
+
+    degree : int, default=3
+        Set degree in kernel function.
+
+    gamma : {'scale', 'auto'} or float, default=1.0
+        Kernel coefficient determining the influence of individual training samples:
+        - 'scale': 1 / (n_features * X.var())
+        - 'auto': 1 / n_features
+        - float: Must be non-negative.
+
+    coef0 : float, default=0
+        Set coef0 in kernel function.
+
+    shrinking : bool, default=True
+        Set whether to use the shrinking heuristics.
+
+    tol : float, default=0.001
+        Set tolerance of termination criterion.
+
+    cache_size : int, default=100
+        Set cache memory size in MB.
+
+    Attributes
+    ----------
+    classes_ : ndarray of shape (n_classes,)
+        Array that contains all different class labels found in the original dataset.
+
+    model_ : object
+        Fitted estimator.
+
+    References
+    ----------
+    .. [1] H.-T. Lin and L. Li, "Reduction from cost-sensitive ordinal ranking to
+           weighted binary classification", Neural Computation, vol. 24, no. 5, pp.
+           1329-1367, 2012, http://10.1162/NECO_a_00265
+
+    .. [2] P.A. Gutiérrez, M. Pérez-Ortiz, J. Sánchez-Monedero, F. Fernández-Navarro
+           and C. Hervás-Martínez, "Ordinal regression methods: survey and
+           experimental study", IEEE Transactions on Knowledge and Data Engineering,
+           Vol. 28. Issue 1, 2016,
+           https://doi.org/10.1109/TKDE.2015.2457911
+
+    """
+
+    _parameter_constraints: dict = {
+        "C": [Interval(Real, 0.0, None, closed="neither")],
+        "kernel": [
+            StrOptions(
+                {
+                    "linear",
+                    "poly",
+                    "rbf",
+                    "sigmoid",
+                    "stump",
+                    "perceptron",
+                    "laplacian",
+                    "exponential",
+                    "precomputed",
+                }
+            )
+        ],
+        "degree": [Interval(Integral, 0, None, closed="left")],
+        "gamma": [
+            StrOptions({"scale", "auto"}),
+            Interval(Real, 0.0, None, closed="neither"),
+        ],
+        "coef0": [Interval(Real, None, None, closed="neither")],
+        "shrinking": ["boolean"],
+        "tol": [Interval(Real, 0.0, None, closed="neither")],
+        "cache_size": [Interval(Real, 0.0, None, closed="neither")],
+    }
+
+    def __init__(
+        self,
+        C=1,
+        kernel="rbf",
+        degree=3,
+        gamma="auto",
+        coef0=0,
+        shrinking=True,
+        tol=0.001,
+        cache_size=100,
+    ):
+        self.C = C
+        self.kernel = kernel
+        self.degree = degree
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.shrinking = shrinking
+        self.tol = tol
+        self.cache_size = cache_size
+
+    @_fit_context(prefer_skip_nested_validation=True)
+    def fit(self, X, y):
+        """Fit the model with the training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Training patterns array, where n_samples is the number of samples and
+            n_features is the number of features.
+
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+
+        Raises
+        ------
+        ValueError
+            If parameters are invalid or data has wrong format.
+
+        """
+        # Additional strict validation for boolean parameters
+        if not isinstance(self.shrinking, bool):
+            raise ValueError(
+                f"The 'shrinking' parameter must be of type bool. "
+                f"Got {type(self.shrinking).__name__} instead."
+            )
+
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+        # Store the classes seen during fit
+        self.classes_ = unique_labels(y)
+
+        # Set default gamma value if not specified
+        gamma_value = self.gamma
+        if self.gamma == "auto":
+            gamma_value = 1.0 / X.shape[1]
+        elif self.gamma == "scale":
+            gamma_value = 1.0 / (X.shape[1] * X.var())
+
+        # Map kernel type
+        kernel_type_mapping = {
+            "linear": 0,
+            "poly": 1,
+            "rbf": 2,
+            "sigmoid": 3,
+            "stump": 4,
+            "perceptron": 5,
+            "laplacian": 6,
+            "exponential": 7,
+            "precomputed": 8,
+        }
+        kernel_type = kernel_type_mapping[self.kernel]
+
+        # Fit the model
+        options = "-s 5 -t {} -d {} -g {} -r {} -c {} -m {} -e {} -h {} -q".format(
+            str(kernel_type),
+            str(self.degree),
+            str(gamma_value),
+            str(self.coef0),
+            str(self.C),
+            str(self.cache_size),
+            str(self.tol),
+            str(1 if self.shrinking else 0),
+        )
+        self.model_ = svm.fit(y.tolist(), X.tolist(), options)
+
+        return self
+
+    def predict(self, X):
+        """Perform classification on samples in X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Test patterns array, where n_samples is the number of samples and
+            n_features is the number of features.
+
+        Returns
+        -------
+        y_pred : array, shape (n_samples,)
+            Class labels for samples in X.
+
+        Raises
+        ------
+        NotFittedError
+            If the model is not fitted yet.
+
+        ValueError
+            If input is invalid.
+
+        """
+        # Check is fit had been called
+        check_is_fitted(self, ["model_"])
+
+        # Input validation
+        X = check_array(X)
+
+        y_pred = np.array(svm.predict(X.tolist(), self.model_))
+
+        return y_pred
---
+++ skordinal/utilities/utilities.py
@@ -0,0 +1,450 @@
+"""Utility class for running experiments."""
+
+from collections import OrderedDict
+from copy import deepcopy
+from pathlib import Path
+from time import time
+
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
+
+from skordinal.metrics import compute_metric
+from skordinal.model_selection import load_classifier
+from skordinal.results import Results
+
+
+class Utilities:
+    """Run experiments over N datasets with M different configurations.
+
+    Configurations are composed of a classifier method and different parameters, where
+    it may be multiple values for every one of them.
+
+    Running the main function of this class will perform cross-validation for each
+    partition per dataset-configuration pairs, obtaining the most optimal model, after
+    what will be used to infer the labels for the test sets.
+
+    Parameters
+    ----------
+    general_conf : dict
+        Dictionary containing values needed to run the experiment. It gives this class
+        information about where are located the different datasets, which one are going
+        to be tested, the metrics to use, etc.
+
+    configurations : dict
+        Dictionary in which are stated the different classifiers to build methods upon
+        the selected datasets, as well as the different values for the hyper-parameters
+        used to optimize the model during cross-validation phase.
+
+    verbose : bool
+        Variable used for testing purposes. Silences all prints.
+
+    Attributes
+    ----------
+    _results : Results
+        Class used to manage and store all information obtained during the run of an
+        experiment.
+
+    """
+
+    def __init__(self, general_conf, configurations, verbose=True):
+        self.general_conf = deepcopy(general_conf)
+        self.configurations = deepcopy(configurations)
+        self.verbose = verbose
+
+    def run_experiment(self):
+        """Run an experiment. Main method of this framework.
+
+        Loads all datasets, which can be fragmented in partitions. Builds a model per
+        partition, using cross-validation to find the optimal values among the
+        hyper-parameters to compare from.
+
+        Uses the built model to get train and test metrics, storing all the information
+        into a Results object.
+
+        Raises
+        ------
+        ValueError
+            If the dataset list is inconsistent.
+
+        AttributeError
+            If the input preprocessing is unknown.
+
+        TypeError
+            If the parameters for base_classifier must be list.
+
+        """
+        self._results = Results(self.general_conf["output_folder"])
+
+        self._check_dataset_list()
+
+        if self.verbose:
+            print("\n###############################")
+            print("\tRunning Experiment")
+            print("###############################")
+
+        # Iterating over Datasets
+        for x in self.general_conf["datasets"]:
+            dataset_name = x.strip()
+            dataset_path = Path(self.general_conf["basedir"]) / dataset_name
+
+            dataset = self._load_dataset(dataset_path)
+
+            if self.verbose:
+                print("\nRunning", dataset_name, "dataset")
+                print("--------------------------")
+
+            # Iterating over Configurations
+            for conf_name, configuration in self.configurations.items():
+                if self.verbose:
+                    print("Running", conf_name, "...")
+
+                # Iterating over partitions
+                for part_idx, partition in dataset:
+                    if self.verbose:
+                        print("  Running Partition", part_idx)
+
+                    # Normalization or Standardization of the partition if requested
+                    if (
+                        self.general_conf["input_preprocessing"].strip().lower()
+                        == "norm"
+                    ):
+                        partition["train_inputs"], partition["test_inputs"] = (
+                            self._normalize_data(
+                                partition["train_inputs"], partition["test_inputs"]
+                            )
+                        )
+                    elif (
+                        self.general_conf["input_preprocessing"].strip().lower()
+                        == "std"
+                    ):
+                        partition["train_inputs"], partition["test_inputs"] = (
+                            self._standardize_data(
+                                partition["train_inputs"], partition["test_inputs"]
+                            )
+                        )
+
+                    elif self.general_conf["input_preprocessing"].strip().lower() != "":
+                        raise AttributeError(
+                            "Input preprocessing named '%s' unknown"
+                            % self.general_conf["input_preprocessing"].strip().lower()
+                        )
+
+                    optimal_estimator = self._get_optimal_estimator(
+                        partition["train_inputs"],
+                        partition["train_outputs"],
+                        configuration["classifier"],
+                        configuration["parameters"],
+                    )
+
+                    # Getting train and test predictions
+                    train_predicted_y = optimal_estimator.predict(
+                        partition["train_inputs"]
+                    )
+
+                    test_predicted_y = None
+                    elapsed = np.nan
+                    if "test_outputs" in partition:
+                        start = time()
+                        test_predicted_y = optimal_estimator.predict(
+                            partition["test_inputs"]
+                        )
+                        elapsed = time() - start
+
+                    # Obtaining train and test metrics values.
+                    train_metrics = OrderedDict()
+                    test_metrics = OrderedDict()
+                    for metric_name in self.general_conf["metrics"]:
+                        # Get train scores
+                        train_score = compute_metric(
+                            metric_name,
+                            partition["train_outputs"],
+                            train_predicted_y,
+                        )
+                        train_metrics[metric_name.strip() + "_train"] = train_score
+
+                        # Get test scores
+                        test_metrics[metric_name.strip() + "_test"] = np.nan
+                        if "test_outputs" in partition:
+                            test_score = compute_metric(
+                                metric_name, partition["test_outputs"], test_predicted_y
+                            )
+                            test_metrics[metric_name.strip() + "_test"] = test_score
+
+                    # Cross-validation was performed to tune hyper-parameters
+                    if isinstance(optimal_estimator, GridSearchCV):
+                        train_metrics["cv_time_train"] = optimal_estimator.cv_results_[
+                            "mean_fit_time"
+                        ].mean()
+                        test_metrics["cv_time_test"] = optimal_estimator.cv_results_[
+                            "mean_score_time"
+                        ].mean()
+                        train_metrics["time_train"] = optimal_estimator.refit_time_
+                        test_metrics["time_test"] = elapsed
+
+                    else:
+                        optimal_estimator.best_params_ = configuration["parameters"]
+                        optimal_estimator.best_estimator_ = optimal_estimator
+
+                        train_metrics["cv_time_train"] = np.nan
+                        test_metrics["cv_time_test"] = np.nan
+                        train_metrics["time_train"] = optimal_estimator.refit_time_
+                        test_metrics["time_test"] = elapsed
+
+                    # Saving the results for this partition
+                    self._results.add_record(
+                        part_idx,
+                        optimal_estimator.best_params_,
+                        optimal_estimator.best_estimator_,
+                        {"dataset": dataset_name, "config": conf_name},
+                        {"train": train_metrics, "test": test_metrics},
+                        {"train": train_predicted_y, "test": test_predicted_y},
+                    )
+
+    def _load_dataset(self, dataset_path):
+        """Load all dataset's files, divided into train and test.
+
+        Parameters
+        ----------
+        dataset_path : Path
+            Path to dataset folder.
+
+        Returns
+        -------
+        partition_list : list of tuples
+            List of partitions found inside a dataset folder. Each partition is stored
+            into a dictionary, disjoining train and test inputs and outputs.
+
+        Raises
+        ------
+        ValueError
+            If the dataset path does not exist.
+
+        RuntimeError
+            If a partition is found without train files.
+
+        """
+
+        def get_partition_index(filename):
+            # Extracts the index between the last "_" and ".csv"
+            return filename.rsplit("_", 1)[-1].replace(".csv", "")
+
+        try:
+            partition_list = {
+                get_partition_index(filename.name): {}
+                for filename in dataset_path.iterdir()
+                if filename.name.startswith("train_")
+            }
+
+            # Loading each dataset
+            for filename in dataset_path.iterdir():
+                if filename.name.startswith("train_"):
+                    idx = get_partition_index(filename.name)
+                    train_inputs, train_outputs = self._read_file(filename)
+                    partition_list[idx]["train_inputs"] = train_inputs
+                    partition_list[idx]["train_outputs"] = train_outputs
+
+                elif filename.name.startswith("test_"):
+                    idx = get_partition_index(filename.name)
+                    test_inputs, test_outputs = self._read_file(filename)
+                    partition_list[idx]["test_inputs"] = test_inputs
+                    partition_list[idx]["test_outputs"] = test_outputs
+
+        except OSError:
+            raise ValueError(f"No such file or directory: '{dataset_path}'")
+
+        except KeyError:
+            raise RuntimeError(
+                f"Found partition without train files: partition {filename.name}"
+            )
+
+        # Saving partitions as a sorted list of (index, partition) tuples
+        partition_list = sorted(
+            partition_list.items(),
+            key=lambda t: int(t[0]) if t[0].lstrip("-").isdigit() else t[0],
+        )
+
+        return partition_list
+
+    def _read_file(self, filename):
+        """Read a CSV containing partitions, or full datasets.
+
+        Train and test files must be previously divided for the experiment to run.
+
+        Parameters
+        ----------
+        filename : str or Path
+            Full path to train or test file.
+
+        Returns
+        -------
+        inputs : {array-like, sparse-matrix} of shape (n_samples, n_features)
+            Vector of sample's features.
+
+        outputs : array-like of shape (n_samples)
+            Target vector relative to inputs.
+
+        """
+        # Separator is automatically found
+        f = pd.read_csv(filename, header=None, engine="python")
+
+        inputs = f.values[:, 0:(-1)]
+        outputs = f.values[:, (-1)]
+
+        return inputs, outputs
+
+    def _check_dataset_list(self):
+        """Check if there is some inconsistency in the dataset list.
+
+        It also simplifies running all datasets inside one folder.
+
+        Raises
+        ------
+        ValueError
+            If the dataset list is inconsistent or contains non-string values.
+
+        """
+        base_path = Path(self.general_conf["basedir"])
+        dataset_list = self.general_conf["datasets"]
+
+        # Check if home path is shortened
+        if str(base_path).startswith("~"):
+            base_path = Path.home() / str(base_path)[1:]
+
+        # Check if 'all' is the only value, and if it is, expand it
+        if len(dataset_list) == 1 and dataset_list[0] == "all":
+            dataset_list = [item.name for item in base_path.iterdir() if item.is_dir()]
+
+        elif not all(isinstance(item, str) for item in dataset_list):
+            raise ValueError("Dataset list can only contain strings")
+
+        self.general_conf["basedir"] = str(base_path)
+        self.general_conf["datasets"] = dataset_list
+
+    def _normalize_data(self, train_data, test_data):
+        """Normalize the data.
+
+        Test data normalization will be based on train data.
+
+        Parameters
+        ----------
+        train_data : 2d array
+            Contain the train data features.
+
+        test_data : 2d array
+            Contain the test data features.
+
+        Returns
+        -------
+        train_normalized : np.ndarray
+            Normalized training data.
+
+        test_normalized : np.ndarray
+            Normalized test data.
+
+        """
+        mm_scaler = preprocessing.MinMaxScaler().fit(train_data)
+
+        return mm_scaler.transform(train_data), mm_scaler.transform(test_data)
+
+    def _standardize_data(self, train_data, test_data):
+        """Standardize the data.
+
+        Test data standardization will be based on train data.
+
+        Parameters
+        ----------
+        train_data : 2d array
+            Contain the train data features.
+
+        test_data : 2d array
+            Contain the test data features.
+
+        Returns
+        -------
+        train_standardized : np.ndarray
+            Standardized training data.
+
+        test_standardized : np.ndarray
+            Standardized test data.
+
+        """
+        std_scaler = preprocessing.StandardScaler().fit(train_data)
+
+        return std_scaler.transform(train_data), std_scaler.transform(test_data)
+
+    def _get_optimal_estimator(
+        self, train_inputs, train_outputs, classifier_name, parameters
+    ):
+        """Perform cross-validation over one dataset and configuration.
+
+        Each configuration consists of one classifier and none, one or multiple
+        hyper-parameters, that, in turn, can contain one or multiple values used to
+        optimize the resulting model.
+
+        At the end of cross-validation phase, the model with the specific combination of
+        values from the hyper-parameters that achieved the best metrics from all the
+        combinations will remain.
+
+        Parameters
+        ----------
+        train_inputs : {array-like, sparse-matrix} of shape (n_samples, n_features)
+            Vector of features for each sample for this dataset.
+
+        train_outputs : array-like of shape (n_samples)
+            Target vector relative to train_inputs.
+
+        classifier_name : str
+            Name of the classification algorithm being employed.
+
+        parameters : dict
+            Dictionary containing parameters to optimize as keys, and the list of
+            values that we want to compare as values.
+
+        Returns
+        -------
+        optimal: GridSearchCV object or classifier object
+            An already fitted model of the given classifier, with the best found
+            parameters after cross-validation. If cross-validation is not needed, it will
+            return the classifier model already trained.
+
+        Raises
+        ------
+        AttributeError
+            If the metric name is not found or cv_metric is not a string.
+
+        """
+        estimator = load_classifier(
+            classifier_name=classifier_name,
+            random_state=np.random.get_state()[1][0],
+            n_jobs=self.general_conf.get("jobs", 1),
+            cv_n_folds=self.general_conf.get("hyperparam_cv_nfolds", 3),
+            cv_metric=self.general_conf.get("cv_metric", "mae"),
+            param_grid=parameters,
+        )
+
+        start = time()
+        estimator.fit(train_inputs, train_outputs)
+        elapsed = time() - start
+
+        if not isinstance(estimator, GridSearchCV):
+            estimator.refit_time_ = elapsed
+            estimator.best_params_ = parameters
+            estimator.best_estimator_ = estimator
+
+        return estimator
+
+    def write_report(self):
+        """Save summarized information about experiment through Results class."""
+        if self.verbose:
+            print("\nSaving Results...")
+
+        # Names of each metric used (plus computational times)
+        metrics_names = [x.strip().lower() for x in self.general_conf["metrics"]] + [
+            "cv_time",
+            "time",
+        ]
+
+        # Saving results through Results class
+        self._results.save_summaries(metrics_names)
