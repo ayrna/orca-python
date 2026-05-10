7f485aeaa6a673bdfaf27876061f6ca3698a46c2
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-05-10 17:47:22 +0200

    ENH: Add ordinal cumulative coding utilities

--- skordinal/preprocessing/__init__.py
+++ skordinal/preprocessing/__init__.py
@@ -1,5 +1,17 @@
-"""Preprocessing module."""
+"""Ordinal preprocessing utilities."""
 
+from ._encodings import (
+    binary_cumulative_to_ordinal,
+    build_coding_matrix,
+    ordinal_to_binary_cumulative,
+)
 from ._scalers import apply_scaling, minmax_scale, standardize
 
-__all__ = ["apply_scaling", "minmax_scale", "standardize"]
+__all__ = [
+    "apply_scaling",
+    "binary_cumulative_to_ordinal",
+    "build_coding_matrix",
+    "minmax_scale",
+    "ordinal_to_binary_cumulative",
+    "standardize",
+]
